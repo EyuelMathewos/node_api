89ffa573160efb828f8a06fbf598cc8ae7f28fd7
Amanuel Agegnehu Bekele <amanuelagegnehu2@gmail.com>
2022-07-19 16:10:09 +0300

    updated schema

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -6,8 +6,8 @@ const prisma = new PrismaClient();
 export const create = async (userData: any) => {
     const result = await prisma.user.create({
         data: {
-            firstName: userData.firstName,
-            lastName: userData.lastName,
+            firstname: userData.firstName,
+            lastname: userData.lastName,
             email: userData.email,
             password: userData.password,
         }
