5d0f8f1217f78bcbc07d2cd17afd618c53489339
EyuelMathewos <eyuelmathewos@gmail.com>
2022-07-19 22:06:16 +0300

    cookie setup

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,30 @@
-import express from 'express';
+import express, { Request } from 'express';
 const app = express();
-const path = require('path');
+const session = require('express-session')
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+const { PrismaClient } = require('@prisma/client');
 
-app.get("/", (req, res) => { 
+interface CustomRequest extends Request {
+  session ? : any
+}
+app.use(express.urlencoded({ extended: false }))
+app.use(session({
+  secret: 'thisismysecretkey',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: true },
+  store: new PrismaSessionStore(
+    new PrismaClient(),
+    {
+      checkPeriod: 2 * 60 * 1000,  //ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }
+  )
+})
+);
+
+app.get("/", (req: CustomRequest, res) => { 
   res.send("Exporess is working"); 
 });
 
