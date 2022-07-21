a4e4e8a03445d3c422f9bb8befdd5062d837581b
EyuelMathewos <eyuelmathewos@gmail.com>
2022-07-21 17:09:49 +0300

    new package added localpassport configured

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,68 @@
-import express, { Request } from 'express';
-import { NextFunction, Response } from 'express-serve-static-core';
+import express, {
+  Request
+} from 'express';
 import userRouter from './routes/userRouter';
 const app = express();
 const session = require('express-session')
-const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
-const { PrismaClient } = require('@prisma/client');
+const pgSession = require('connect-pg-simple')(session);
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 
 interface CustomRequest extends Request {
-  session?: any
+  session: any
 }
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
+
+app.use(passport.initialize());
+const options = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DATABASE_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  createTableIfMissing: true
+}
+const pgStore = new pgSession(options)
+
 app.use(session({
-  secret: process.env.SECRET,
+  secret: 'process.env.SECRET',
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: true },
-  store: new PrismaSessionStore(
-    new PrismaClient(),
-    {
-      checkPeriod: 2 * 60 * 1000,  //ms
-      dbRecordIdIsSessionId: true,
-      dbRecordIdFunction: undefined,
-    }
-  )
-})
-);
+  cookie: {
+    maxAge: 2 * 60 * 1000,
+    secure: false
+  },
+  store: pgStore
 
-function isAuth(req: CustomRequest, res: Response, next: NextFunction) {
-  const loggedinUser = req.session.isAuth;
-  // console.log(loggedinUser);
-  next()
-}
+}));
 
-app.use(isAuth);
-app.get("/", (req: CustomRequest, res) => {
-  res.send("Exporess is working");
+app.use(passport.session());
+passport.use(new LocalStrategy(
+  async function(username: any, password: any, done: any) {
+     const user = {id: 1};
+        done(null,  user);
+   }
+ ));
+ 
+ passport.serializeUser(function(id: any, cb: any) {
+   process.nextTick(function() {
+     return cb(null, id);
+   });
+ });
+ 
+ passport.deserializeUser(function(id: any, cb: any) {
+   process.nextTick(function() {
+     return cb(null, id);
+   });
+ });
+
+
+app.use(passport.authenticate('session'))
+app.get("/", (req, res) => { 
+  res.send("Exporess is working"); 
 });
 
 app.use("/api/v1/user", userRouter);
